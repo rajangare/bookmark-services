b2ddd1ded49aace451e28870f058caed971d2d9f
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-06 17:14:01 +0530

    Configurations and Eureka client

--- bootstrap/src/main/java/com/assignment/BookmarkApplication.java
+++ bootstrap/src/main/java/com/assignment/BookmarkApplication.java
@@ -3,11 +3,13 @@ package com.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookmarkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookmarkApplication.class, args);

11627b22ea0e874def9e7a2d22dba86ff0f32bf3
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-07 19:47:20 +0530

    Running project

--- bootstrap/src/main/java/com/assignment/BookmarkApplication.java
+++ bootstrap/src/main/java/com/assignment/BookmarkApplication.java
@@ -1,11 +1,14 @@
 package com.assignment;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class })
 public class BookmarkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookmarkApplication.class, args);

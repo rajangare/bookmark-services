7dd2b6015b216c733c495e5c9e625eaac8764045
Rajkumar ANGARE <rajangare@gmail.com>
2020-09-07 20:18:44 +0530

    Swagger Config - Running project

--- bootstrap/src/main/java/com/assignment/BookmarkApplication.java
+++ bootstrap/src/main/java/com/assignment/BookmarkApplication.java
@@ -1,14 +1,13 @@
 package com.assignment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class })
+@EnableSwagger2
 public class BookmarkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookmarkApplication.class, args);

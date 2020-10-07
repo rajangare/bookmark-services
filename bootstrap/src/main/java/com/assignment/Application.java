0dc4f3497d4f8a72b48973812bf420e4317f40de
amit tomar <amit.tomar1101@gmail.com>
2020-10-07 11:15:59 +0530

    Update Application.java
    Added change to check Ci pipeline.

--- bootstrap/src/main/java/com/assignment/Application.java
+++ bootstrap/src/main/java/com/assignment/Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class Application  {
     public static void main(String[] args) {
+        System.out.println("Application Started !!!");
         SpringApplication.run(Application.class, args);
     }
 }
